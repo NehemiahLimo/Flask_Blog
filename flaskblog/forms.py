791c83958f5a7a3f9118e4c0045246562762f381
NehemiahLimo <nehemiahlimo02@gmail.com>
2019-05-22 21:25:36 +0300

    Some few modification

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -4,6 +4,8 @@ from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
+
+
 class RegistrationForm(FlaskForm):
     username= StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
     email=StringField('Email', validators=[DataRequired(), Email()])
