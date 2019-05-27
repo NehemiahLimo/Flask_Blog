e9761aa74b4d13cef2a9ab290223f75b1aa562b2
Nehemiah Cheburet <cheburet.nehemiah@intsoftkenya.co.ke>
2019-05-27 14:19:25 +0300

    Fixed token-based password reset

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -17,8 +17,8 @@ login_manager.login_message_category= 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER', 'nehemiahlimo02@gmail.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS', 'Provide you email password here')
 mail = Mail(app)
 
 from flaskblog import routes
